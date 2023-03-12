f7eeeee059eabc2b902e96108b281b255e79de2e
Slava Kuryaev <92830706+necrosskull@users.noreply.github.com>
2023-03-13 02:30:22 +0300

    !!!unstable inline_mode testing

--- bot/main.py
+++ bot/main.py
@@ -596,6 +596,91 @@ def got_back_handler(update: Update, context: CallbackContext):
         return send_week_selector(update, context)
 
 
+def inlinequery(update: Update, context: CallbackContext):
+    """
+    Обработчик инлайн запросов
+    Создает Inline отображение
+    """
+    query = update.inline_query.query
+    if not query:
+        return
+    query = query.title()
+    if " " not in query:
+        query += " "
+    teacher_schedule = fetch_schedule_by_name(query)
+    if teacher_schedule is None:
+        return
+    surnames = check_same_surnames(teacher_schedule, query)
+    if len(surnames) == 0:
+        return
+    inline_results = []
+    decoded_surnames = decode_teachers(surnames)
+    userid = str(update.inline_query.from_user.id)
+    for surname, decoded_surname in zip(surnames, decoded_surnames):
+        inline_results.append(InlineQueryResultArticle(
+            id=surname,
+            title=decoded_surname,
+            description="Нажми, чтобы посмотреть расписание",
+            input_message_content=InputTextMessageContent(
+                message_text=f"Выбран преподаватель: {decoded_surname}!"
+            ),
+            reply_markup=construct_weeks_markup(),
+
+        ))
+    update.inline_query.answer(inline_results, cache_time=10, is_personal=True)
+
+
+def answer_inline_handler(update: Update, context: CallbackContext):
+    """
+    В случае отработки события ChosenInlineHandler запоминает выбранного преподавателя
+    и выставляет текущий шаг Inline запроса на ask_day
+    """
+    if update.chosen_inline_result is not None:
+        context.user_data["teacher"] = update.chosen_inline_result.result_id
+        context.user_data["inline_step"] = EInlineStep.ask_week
+        return
+
+
+def inline_dispatcher(update: Update, context: CallbackContext):
+    """
+    Обработка вызовов в чатах на основании Callback вызова
+    """
+    if "inline_step" not in context.user_data:
+        deny_inline_usage(update)
+        return
+    status = context.user_data["inline_step"]
+    if status == EInlineStep.completed or status == EInlineStep.ask_teacher:
+        deny_inline_usage(update)
+        return
+    if status == EInlineStep.ask_week:
+        context.user_data["schedule"] = fetch_schedule_by_name(context.user_data["teacher"])
+        target = got_week_handler(update, context)
+        if target == GETDAY:
+            context.user_data["inline_step"] = EInlineStep.ask_day
+        elif target == GETWEEK or target == ConversationHandler.END:
+            return
+        else:
+            update.callback_query.edit_message_text("Вызовите бота снова, указав нужного преподавателя")
+            context.user_data["inline_step"] = EInlineStep.ask_teacher
+        return
+    if status == EInlineStep.ask_day:
+        target = got_day_handler(update, context)
+        if target == GETWEEK:
+            context.user_data["inline_step"] = EInlineStep.ask_week
+        elif target != GETDAY:
+            context.user_data["inline_step"] = EInlineStep.completed
+        return
+
+
+def deny_inline_usage(update: Update):
+    """
+    Показывает предупреждение пользователю, если он не может использовать имеющийся Inline вызов
+    """
+    update.callback_query.answer(text="Вы не можете использовать это меню, т.к. его вызвал другой человек",
+                                 show_alert=True)
+    return
+
+
 def main():
     conv_handler = ConversationHandler(
         entry_points=[
@@ -616,6 +701,11 @@ def main():
     )
 
     dispatcher.add_handler(conv_handler)
+
+    dispatcher.add_handler(InlineQueryHandler(inlinequery, run_async=True))
+    dispatcher.add_handler(ChosenInlineResultHandler(answer_inline_handler, run_async=True))
+    dispatcher.add_handler(CallbackQueryHandler(inline_dispatcher, run_async=True))
+
     dispatcher.add_handler(CommandHandler("help", start, run_async=True))
     updater.start_polling()
 
