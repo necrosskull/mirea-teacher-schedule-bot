b602b3429a543280435b6a3c87cfcb795aafdb24
Slava Kuryaev <92830706+necrosskull@users.noreply.github.com>
2023-05-10 23:36:04 +0300

    fix: decode_teachers
    теперь если апи cms ничего не возвращает, берётся rawName

--- bot/main.py
+++ bot/main.py
@@ -410,30 +410,30 @@ def decode_teachers(rawNames):
     response = requests.get("https://cms.mirea.ninja/api/get-full-teacher-name", headers=headers, params=params)
     if response.status_code == 200:
         data = response.json()
+        if data:
+            decoded_names = {}
+            for names in data:
+                if len(names["possibleFullNames"]) == 1:
+                    decomposed_name = names["possibleFullNames"][0]
+                    name = []
+                    if surname := decomposed_name.get("lastName"):
+                        name.append(surname)
+                    if first_name := decomposed_name.get("firstName"):
+                        name.append(first_name)
+                    if middle_name := decomposed_name.get("middleName"):
+                        name.append(middle_name)
+                    name = " ".join(name)
+                    raw_name = names["rawName"]
+                    decoded_names[raw_name] = name
 
-        decoded_names = []
-        for names in data:
-            if len(names["possibleFullNames"]) == 1:
-                decomposed_name = names["possibleFullNames"][0]
-                name = []
-                if surname := decomposed_name.get("lastName"):
-                    name.append(surname)
-                if first_name := decomposed_name.get("firstName"):
-                    name.append(first_name)
-                if middle_name := decomposed_name.get("middleName"):
-                    name.append(middle_name)
-                name = " ".join(name)
-            else:
-                name = names["rawName"]
-            decoded_names.append(name)
+            # Create a list of decoded names in the same order as raw names
+            decoded_list = [decoded_names[raw_name] if raw_name in decoded_names else raw_name for raw_name in rawNames]
 
-        decoded_names = decoded_names
+        else:
+            decoded_list = rawNames
     else:
-        decoded_names = rawNames
-    if len(decoded_names) == len(rawNames):
-        return decoded_names
-    else:
-        return rawNames
+        decoded_list = rawNames
+    return decoded_list
 
 
 def prepare_teacher_markup(teachers):
