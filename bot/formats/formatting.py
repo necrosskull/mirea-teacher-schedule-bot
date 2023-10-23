a2301c13c9f22036795692ed3584d2e685f51ab3
Slava Kuryaev <92830706+necrosskull@users.noreply.github.com>
2023-10-23 15:28:52 +0300

    update: drop replace_letters_in_teacher_name

--- bot/formats/formatting.py
+++ bot/formats/formatting.py
@@ -145,7 +145,7 @@ def normalize_teachername(raw_teacher_name: str):
     @param raw_teacher_name: Ввод пользователя
     @return: Фамилия начинаяющая с большой буквы и с пробелом в конце
     """
-    teacher = raw_teacher_name.title()
+    teacher = raw_teacher_name.title().replace('ё', 'е')
     name_parts = teacher.split()
 
     if len(name_parts) > 1:
@@ -166,34 +166,17 @@ def normalize_teachername(raw_teacher_name: str):
     return teacher
 
 
-def replace_letters_in_teacher_name(teacher_name):
-    """
-    Заменяет 'ё' на 'е' или 'е' на 'ё' в фамилии учителя.
-    :param teacher_name: Фамилия
-    :return: modified_name - Фамилия с замененными буквами
-    """
-
-    if "ё" in teacher_name:
-        modified_name = teacher_name.replace("ё", "е")
-    elif "е" in teacher_name:
-        modified_name = teacher_name.replace("е", "ё")
-    else:
-        modified_name = teacher_name
-
-    return modified_name
-
-
 def check_same_surnames(teacher_schedule, surname):
     """
     Проверяет имеющихся в JSON преподавателей.
-    В случае нахождения однофамильца, но сдругим именем или фамилией заносит в список surnames
+    В случае нахождения однофамильца, но с другим именем или фамилией заносит в список surnames
     :param teacher_schedule: JSON строка расписания
     :param surname: Строка фильтрации, например фамилия
     :return: surnames - лист ФИО преподавателей
     """
     surnames = []
     for teacher in teacher_schedule:
-        if surname in teacher['name']:
+        if surname in teacher['name'].replace('ё', 'е'):
             if teacher['name'][-1] != ".":
                 teacher['name'] += "."
 
